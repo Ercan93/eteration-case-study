25eabbed527e055b46ddd80f2580256999bab68a
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-01-04 20:10:26 +0300

    Refactor NotFound component to include a user-friendly error page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom";
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
+    <div className="container">
+      <div className="row mt-5 text-center">
+        <div className="col-12">
+          <h1 className="fs-1 fw-bold text-danger">404</h1>
+          <p className="fs-2 fw-semibold">Page not found</p>
+          <p className="fs-5">The page you are looking for does not exist.</p>
+          <button
+            className="btn btn-primary mt-2"
+            onClick={() => navigate("/")}
+          >
+            Back to Home
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
