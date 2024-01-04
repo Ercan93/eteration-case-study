234b8e47365b340821ceace8374a70dad18e19d9
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-01-04 20:35:32 +0300

    Add Accordion component with styles

---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import downIcon from "../../assets/static/down.svg";
+import "./Accordion.css";
+
+const Accordion = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="accordion-wrapper border my-2 rounded">
+      <div
+        className={`accordion-title d-flex align-items-center justify-content-between py-2 px-3 fw-semibold ${
+          isOpen ? "open" : ""
+        }`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p className="mb-0">{title}</p>
+        <img src={downIcon} width="40" height="40" alt="down" className="accordion-icon" />
+      </div>
+      <div className={`accordion-content-wrapper ${isOpen ? "open" : ""}`}>
+        <div className="accordion-content">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
