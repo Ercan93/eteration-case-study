2a01df8fec01d6a72a2635ca9e707207c1917439
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-01-04 16:34:18 +0300

    Add SORT_VALUES constant

---
+++ src/helper/constants.js
@@ -0,0 +1,7 @@
+export const SORT_VALUES = {
+  featured: "Featured",
+  "old-to-new": "Old to New",
+  "new-to-old": "New to Old",
+  "price-highest": "Price High to Low",
+  "price-lowest": "Price Low to High",
+};
